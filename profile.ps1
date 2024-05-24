0647a9bfa984013c79ed805f5dacb4033346bd70
John Børge Holen-Tjelta <johnbt@outlook.com>
2024-05-24 15:06:16 +0200

    add goto function

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,14 @@
 Set-Alias touch New-Item
 Set-Alias grep sls
+
+function goto {
+    param($directory)
+    if ($directory -eq "jotta") {
+        Set-Location "C:\Users\$env:USERNAME\Jottacloud"
+    }
+    elseif ($directory -eq "git") {
+        Set-Location "C:\git"
+    } else {
+        Set-Location $directory
+    }
+}
