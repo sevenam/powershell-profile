5094a8c96938853280fd060cd28befa9b3106237
John Børge Holen-Tjelta <johnbt@outlook.com>
2025-06-09 23:23:02 +0200

    Update private-profile.ps1

--- private-profile.ps1
+++ private-profile.ps1
@@ -106,7 +106,7 @@ function goto {
     if ($directory -eq "jotta") {
         Set-Location "C:\Users\$env:USERNAME\Jottacloud"
     } elseif ($directory -eq "git") {
-        Set-Location "C:\git"
+        Set-Location "D:\git"
     } elseif ($directory -eq "notes") {
         Set-Location "C:\Users\$env:USERNAME\Jottacloud\notes"
     } else {
